1c6ea70dece698668555dd080c3ee6724e7ec9e0
GlennFolker <glenn.folk08@gmail.com>
2024-05-05 14:58:28 +0700

    Bluetooth-requesting

--- src/com/github/glennfolker/kirrc/KIRActivity.kt
+++ src/com/github/glennfolker/kirrc/KIRActivity.kt
@@ -1,5 +1,7 @@
 package com.github.glennfolker.kirrc
 
+import android.bluetooth.*
+import android.content.*
 import android.os.*
 import androidx.activity.result.*
 import androidx.activity.result.contract.ActivityResultContracts.*
@@ -8,23 +10,43 @@ import androidx.fragment.app.*
 import com.github.glennfolker.kirrc.frag.*
 
 class KIRActivity: AppCompatActivity(R.layout.act_kir) {
-    private lateinit var reqBluetooth: ActivityResultLauncher<String>
+    private lateinit var bluetoothAdapter: BluetoothAdapter
+
+    private val enableBluetooth: ActivityResultLauncher<Intent> = registerForActivityResult(StartActivityForResult()) { result ->
+        if(result.resultCode == RESULT_OK) {
+            
+        } else {
+
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
         super.onCreate(savedInstanceState, persistentState)
         if(savedInstanceState == null) {
-            reqBluetooth = registerForActivityResult(RequestPermission()) { isGranted ->
-                if(isGranted) {
-
-                } else {
-
-                }
-            }
-
             supportFragmentManager.commit {
                 setReorderingAllowed(true)
                 add<RequestFrag>(R.id.view_request)
             }
         }
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        val manager = getSystemService(BLUETOOTH_SERVICE) as BluetoothManager
+        val adapter = manager.adapter
+        if(adapter == null) {
+            AlertDialog.Builder(this)
+                .setMessage(R.string.bluetooth_unsupported)
+                .setOnDismissListener { finishAndRemoveTask() }
+                .create()
+                .show()
+        }
+
+        bluetoothAdapter = adapter
+    }
+
+    fun requestBluetooth() {
+        enableBluetooth.launch(Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE))
     }
 }
--- src/com/github/glennfolker/kirrc/frag/RequestFrag.kt
+++ src/com/github/glennfolker/kirrc/frag/RequestFrag.kt
@@ -1,8 +1,18 @@
 package com.github.glennfolker.kirrc.frag
 
-import androidx.fragment.app.Fragment
+import android.os.*
+import android.view.*
+import android.widget.*
+import androidx.fragment.app.*
 import com.github.glennfolker.kirrc.*
 
 class RequestFrag: Fragment(R.layout.frag_request) {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view = super.onCreateView(inflater, container, savedInstanceState)
+        view?.findViewById<Button>(R.id.btn_try_pair)?.setOnClickListener {
+            (activity as KIRActivity).requestBluetooth()
+        }
 
+        return view
+    }
 }
