a68fd8ddd937f36c68bf73c69d6086039626a3f6
GlennFolker <glenn.folk08@gmail.com>
2024-05-11 19:42:12 +0700

    Turns out it wasn't even Bluetooth LE...

--- kir_rc_peripheral/kir_rc_peripheral.ino
+++ kir_rc_peripheral/kir_rc_peripheral.ino
@@ -1,10 +1,41 @@
+#include <SoftwareSerial.h>
 
+enum Pins {
+    BT_TRANSMIT = 5,
+    BT_RECEIVE = 3
+};
+
+SoftwareSerial BtSerial(Pins::BT_RECEIVE, Pins::BT_TRANSMIT);
 
 void setup() {
+    BtSerial.begin(38400);
     Serial.begin(9600);
-    Serial.println("Initializing KIR-RC Arduino peripheral...");
 }
 
 void loop() {
-    
+    if(BtSerial.available() < 1) return;
+
+    int packet = BtSerial.read();
+    int tx = packet & 0b0011, ty = (packet & 0b1100) >> 2;
+
+    // For `x`, 1 means "turn right" while -1 means "turn left".
+    // For `y`, 1 means "forward", while -1 means... I don't know, that'll probably be refactored.
+    int x, y;
+    switch(tx) {
+        case 0b01: x = 1; break;
+        case 0b10: x = -1; break;
+        default: x = 0;
+    }
+
+    switch(ty) {
+        case 0b01: y = 1; break;
+        case 0b10: y = -1; break;
+        default: y = 0;
+    }
+
+    Serial.print("(x: ");
+    Serial.print(x);
+    Serial.print(", y: ");
+    Serial.print(y);
+    Serial.println(")");
 }
--- src/com/github/glennfolker/kirrc/KIRActivity.kt
+++ src/com/github/glennfolker/kirrc/KIRActivity.kt
@@ -2,12 +2,10 @@ package com.github.glennfolker.kirrc
 
 import android.Manifest.*
 import android.bluetooth.*
-import android.bluetooth.le.*
 import android.content.*
 import android.content.pm.*
 import android.os.*
 import android.os.Build.*
-import android.util.*
 import androidx.activity.result.*
 import androidx.activity.result.contract.ActivityResultContracts.*
 import androidx.appcompat.app.*
@@ -15,75 +13,32 @@ import androidx.core.app.*
 import androidx.core.content.*
 import androidx.fragment.app.*
 import com.github.glennfolker.kirrc.fragment.*
+import java.io.*
 import java.util.*
 
 class KIRActivity: AppCompatActivity(R.layout.activity_kir), BluetoothConnector {
-    companion object {
-        val COMMAND_SERVICE: UUID = UUID.fromString("0e5332cf-447c-4f20-a331-3b04719e9a91")
-        val COMMAND_CHARACTERISTIC: UUID = UUID.fromString("ceb56614-7633-4609-b552-f6770eea1c1f")
-    }
-
     private lateinit var bluetoothAdapter: BluetoothAdapter
-    private var commandGatt: BluetoothGatt? = null
-    private var commandCharacteristic: BluetoothGattCharacteristic? = null
+    private var bluetoothSocket: BluetoothSocket? = null
+    private var bluetoothOutput: OutputStream? = null
 
-    private var scanning: Boolean = false
-
+    @Suppress("MissingPermission")
     private val enableBluetooth: ActivityResultLauncher<Intent> = registerForActivityResult(StartActivityForResult()) { result ->
         if(
-            result.resultCode == RESULT_OK && !scanning &&
+            result.resultCode == RESULT_OK &&
             (
                 VERSION.SDK_INT < VERSION_CODES.S ||
                 ActivityCompat.checkSelfPermission(this, permission.BLUETOOTH_SCAN) == PackageManager.PERMISSION_GRANTED
             )
         ) {
-            scanning = true
-
-            val load = LoadFragment()
-            load.show(supportFragmentManager, "fragment-load")
-
-            val scanner = bluetoothAdapter.bluetoothLeScanner
             val list = ConnectFragment()
-
-            val callback = object: ScanCallback() {
-                override fun onScanResult(callbackType: Int, result: ScanResult) {
-                    list.adapter.add(result.device)
-                }
-
-                override fun onScanFailed(errorCode: Int) {
-                    Log.e("ScanCallback", "Scanning bluetooth failed, error code $errorCode.")
-                }
-            }
-
             supportFragmentManager.commit {
                 setReorderingAllowed(true)
                 replace(R.id.root_fragment, list)
             }
 
-            @Suppress("MissingPermission")
-            scanner.startScan(
-                listOf(ScanFilter.Builder().setServiceUuid(ParcelUuid(COMMAND_SERVICE)).build()),
-                ScanSettings.Builder()
-                    .setScanMode(ScanSettings.SCAN_MODE_LOW_LATENCY)
-                    .build(),
-                callback
-            )
-
-            Thread {
-                Looper.prepare()
-
-                val looper = Looper.myLooper()!!
-                Handler(looper).postDelayed({
-                    @Suppress("MissingPermission")
-                    scanner.stopScan(callback)
-
-                    looper.quit()
-                    load.dismiss()
-                    scanning = false
-                }, 5000)
-
-                Looper.loop()
-            }.start()
+            bluetoothAdapter.bondedDevices
+                .filter { it.name.contains("KIR") }
+                .forEach { list.adapter.add(it) }
         } else {
             AlertFragment(R.string.bluetooth_denied_request).show(supportFragmentManager, "fragment-bluetooth-denied-request")
         }
@@ -128,67 +83,62 @@ class KIRActivity: AppCompatActivity(R.layout.activity_kir), BluetoothConnector 
         })
     }
 
+    @Suppress("MissingPermission")
     override fun connectBluetooth(device: BluetoothDevice) {
         val load = LoadFragment()
         load.show(supportFragmentManager, "fragment-load")
 
-        @Suppress("MissingPermission")
-        commandGatt = device.connectGatt(this, false, object: BluetoothGattCallback() {
-            override fun onConnectionStateChange(gatt: BluetoothGatt?, status: Int, newState: Int) {
-                if(status == BluetoothGatt.GATT_SUCCESS) {
-                    if(newState == BluetoothProfile.STATE_CONNECTED) {
-                        gatt?.discoverServices()
-                    }
-                } else {
-                    load.dismiss()
+        Thread {
+            try {
+                // The passed UUID is the defined UUID for HC-05 RFCOMM socket(s).
+                val socket = device.createRfcommSocketToServiceRecord(UUID.fromString("00001101-0000-1000-8000-00805F9B34FB"))
+                socket.connect()
 
-                    gatt?.close()
-                    commandGatt = null
-
-                    supportFragmentManager.commit {
-                        setReorderingAllowed(true)
-                        replace<RequestFragment>(R.id.root_fragment)
-                    }
-                }
-            }
-
-            override fun onServicesDiscovered(gatt: BluetoothGatt?, status: Int) {
-                load.dismiss()
-                commandCharacteristic = gatt?.getService(COMMAND_SERVICE)?.getCharacteristic(COMMAND_CHARACTERISTIC)
+                bluetoothOutput = socket.outputStream
+                bluetoothSocket = socket
 
                 supportFragmentManager.commit {
                     setReorderingAllowed(true)
                     replace<ControlFragment>(R.id.root_fragment)
                 }
+            } catch(e: Exception) {
+                //TODO alert can't connect
+
+                bluetoothSocket?.run { close() }
+                bluetoothSocket = null
+                bluetoothOutput = null
             }
-        })
+
+            load.dismiss()
+        }.start()
     }
 
     @Suppress("MissingPermission")
     override fun commandBluetooth(x: Int, y: Int) {
-        commandGatt?.run {
-            commandCharacteristic?.let {
-                val payload = byteArrayOf(
-                    when(x) {
-                        1 -> 0b01
-                        -1 -> 0b10
-                        else -> 0b00
-                    },
-                    when(y) {
-                        1 -> 0b01
-                        -1 -> 0b10
-                        else -> 0b00
-                    }
-                )
+        try {
+            bluetoothOutput?.write(
+                // In `0b1111`, `0b0011` is used as X delta and `0b1100` is used as Y delta.
+                // A value of `0b00` means zer, `0b01` means positive, and `0b10` means negative.
+                when(x) {
+                    1 -> 0b01
+                    -1 -> 0b10
+                    else -> 0b00
+                } or
+                (when(y) {
+                    1 -> 0b01
+                    -1 -> 0b10
+                    else -> 0b00
+                } shl 2)
+            )
+        } catch(e: IOException) {
+            bluetoothSocket?.close()
+            bluetoothSocket = null
+            bluetoothOutput = null
 
-                @Suppress("Deprecation")
-                if(VERSION.SDK_INT >= VERSION_CODES.TIRAMISU) {
-                    writeCharacteristic(it, payload, BluetoothGattCharacteristic.WRITE_TYPE_NO_RESPONSE)
-                } else {
-                    it.writeType = BluetoothGattCharacteristic.WRITE_TYPE_NO_RESPONSE
-                    it.value = payload
-                    writeCharacteristic(it)
-                }
+            //TODO alert disconnected
+            supportFragmentManager.commit {
+                setReorderingAllowed(true)
+                replace<RequestFragment>(R.id.root_fragment)
             }
         }
     }
